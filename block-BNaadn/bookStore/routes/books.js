2c26b63aabdab039375e7edc36a70f15919e261f
aditya <agoswami0708g@gmail.com>
2022-07-05 23:27:32 +0530

    done

--- block-BNaadn/bookStore/routes/books.js
+++ block-BNaadn/bookStore/routes/books.js
@@ -5,18 +5,43 @@ const Author = require("../models/authors")
 const Category = require("../models/category")
 
 
-// routes: /books/new
-
+// get request on /books/new
 router.get("/new",(req,res) =>{
     console.log(req.body)
     res.render("bookForm");
 });
 
-// post request on /new
 
-router.post("/",(req,res,next)=>{
-    
-})
+router.post('/',(req,res,next)=>{
+    Book.create( req.body,(err,books)=>{
+        if(err) return next(err);
+        res.redirect('/books');
+    });
+});
+
+router.get("/", (req, res) => {
+    Book.find({}, (err, book) => {
+        res.render("allbooks", { book: book});
+    });
+});
+
+router.get("/:id", (req, res) => {
+    let id = req.params.id;
+    Book.findById(id)
+        .populate("category")
+        .populate("author")
+        .exec((err, book) => {
+            render("eachbook", { book: book });
+        });
+});
+router.get("/:categoryname/category/", (req, res) => {
+    let categoryname = req.params.categoryname;
+    Category.find({ name: categoryname })
+        .populate("books")
+        .exec((err, categoryData) => {
+        res.render("bookscategory", { category: categoryData });
+        });
+});
 
 
 module.exports = router;
